5918f821f8a992dbc3ba4b9bf333ba1c2559d71a
Jared Eklin <jaredeklin@gmail.com>
2018-06-04 19:40:28 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.broadcast.emit('chat message', 'a user connected')
+
+  socket.on('chat message', (msg) => {
+    socket.broadcast.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('chat message', 'a user disconnected')
+  }) 
+})
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
